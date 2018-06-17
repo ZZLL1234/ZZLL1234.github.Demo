81f6dd1454a79f283fae554bd269a26ba71c8193
zhouliang <zhouliangmet@163.com>
2018-06-17 15:30:03 +0800

    写了一个方法

---
+++ src/main/java/com/china/App.java
@@ -0,0 +1,17 @@
+package com.china;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        App app=new App();
+        app.method();
+    }
+
+    private void method() {
+        System.out.println("第一次使用git提交本地数据");
+    }
+
+}
